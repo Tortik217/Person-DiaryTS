4629950c745a1f7a91ef35bce5e11a8912c56188
tortik217 <ivan.koledov2012@yandex.ru>
2025-11-03 17:51:04 +0300

    some fix

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,7 +1,8 @@
 import type { INote } from "../types/INote";
 import { getIsoDate } from "../hooks/dateUtils";
+import {nanoid} from "nanoid";
 
 export const Notes: INote[] = [
-  { id: 1, text: "Hello, World!", completed: false, date: getIsoDate() },
+  { id: nanoid(5), text: "Hello, World!", completed: false, date: getIsoDate() },
 ];
 // export const Notes: INote[] = [];
