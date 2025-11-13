ee474ae80319d77d4805f9d9c18557713c905b2b
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2025-11-13 13:56:00 +0300

    add succs mes

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,45 +1,53 @@
-import {useState} from "react";
-import {useOutletCtx} from '../../hooks'
+import { useState } from "react";
+import { useOutletCtx } from "../../hooks";
 
 export function FormPage() {
   const [inputValue, setInputValue] = useState<string>("");
+  const [successMessage, setSuccessMessage] = useState<string>("");
 
   const { addNote } = useOutletCtx();
 
   const handleAdd = () => {
     if (inputValue.trim()) {
       addNote(inputValue);
+      setSuccessMessage("Запись успешно создана");
       setInputValue("");
+      setTimeout(() => {
+        setSuccessMessage("");
+      }, 3000);
     }
   };
 
-  // const clearInputDate  = inputValue.trim().toLowerCase();
-
   return (
-      <form
-          action="#"
-          className="form d-flex flex-column gap-2"
-          onSubmit={(e) => e.preventDefault()}
-      >
+    <form
+      action="#"
+      className="form d-flex flex-column gap-2"
+      onSubmit={(e) => e.preventDefault()}
+    >
       <textarea
-          className="form-control textarea-main"
-          name="entry"
-          id="floatingTextarea2"
-          rows={15}
-          cols={50}
-          placeholder="Enter whatever you want :)"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-          required
+        className="form-control textarea-main"
+        name="entry"
+        id="floatingTextarea2"
+        rows={15}
+        cols={50}
+        placeholder="Enter whatever you want :)"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        required
       ></textarea>
-        <button
-            type="button"
-            className="textarea-btn btn btn-primary"
-            onClick={() => handleAdd()}
-            disabled={inputValue.length < 3}
-        >
-          Push
-        </button>
-      </form>
+      <button
+        type="button"
+        className="textarea-btn btn btn-primary"
+        onClick={() => handleAdd()}
+        disabled={inputValue.length < 3}
+      >
+        Push
+      </button>
+      {successMessage && (
+        <div className="successMessage mt-2 fs-1" style={{ color: "#2e7d32" }}>
+          ✅ {successMessage}
+        </div>
+      )}
+    </form>
   );
 }
