5ec5106e7ec48f15fad97f36bb5c2c54c489b908
tortik217 <ivan.koledov2012@yandex.ru>
2025-12-19 13:16:45 +0300

    added json-server

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-import { Header } from "../components/Header/Header";
-import { Outlet } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { Notes } from "../data/notes";
-import { getIsoDate } from "../hooks/dateUtils";
-import type { INote } from "../types/INote";
-import { nanoid } from "nanoid";
-import * as localforage from "localforage";
-=======
 import {Header} from "../components/Header/Header";
 import {Outlet} from "react-router-dom";
 import {useState, useEffect} from "react";
@@ -17,7 +7,6 @@ import type {INote} from "../types/INote";
 import {nanoid} from "nanoid";
 import notesAPI from "../api/notesAPI.ts";
 // import * as localforage from "localforage";
->>>>>>> Stashed changes
 
 export function Root() {
   const [notes, setNotes] = useState<INote[]>(Notes);
@@ -39,22 +28,9 @@ export function Root() {
   useEffect(() => {
     (async () => {
       try {
-<<<<<<< Updated upstream
-        const stored = await localforage.getItem<INote[] | null>(NOTES_KEY);
-
-        //Временно
-        const normalized =
-          Array.isArray(stored) && stored.length > 0 ? stored : Notes;
-        setNotes(normalized);
-        // Временно
-
-        //если нет шаблона
-        // setNotes(Array.isArray(stored) ? stored : Notes);
-=======
         const response = await fetch("http://localhost:3002/notes");
         const json = await response.json();
         setNotes(Array.isArray(json) ? json : []);
->>>>>>> Stashed changes
       } catch (e) {
         console.error("Не удалось загрузить заметки:", e);
         setNotes(Notes);
@@ -74,18 +50,6 @@ export function Root() {
     }
   };
 
-<<<<<<< Updated upstream
-  const editNote = (id: string, newText: string) => {
-    setNotes((prev) =>
-      prev.map((note) => (note.id === id ? { ...note, text: newText } : note))
-    );
-  };
-
-  const toggleCompleted = (id: string) =>
-    setNotes((list) =>
-      list.map((n) => (n.id === id ? { ...n, completed: !n.completed } : n))
-    );
-=======
   const editNote = async (id: string, newText: string) => {
     try {
       const updated = await notesAPI.edit(id, newText);
@@ -113,16 +77,11 @@ export function Root() {
     }
   };
 
->>>>>>> Stashed changes
 
   return (
-    <div className="main d-flex flex-column justify-content-center align-items-center">
-      <div className="main-section p-3">
-        <Header />
-        <Outlet
-          context={{ notes, addNote, removeNote, editNote, toggleCompleted }}
-        />
+      <div className="main d-flex flex-column justify-content-center align-items-center">
+        <Header/>
+        <Outlet context={{notes, addNote, removeNote, editNote, toggleCompleted}}/>
       </div>
-    </div>
   );
 }
